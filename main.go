560c8e1ca0e01190cab142d012a346d39cd2c718
catGirl_seeker <danildenisov572@gmail.com>
2026-01-07 23:54:20 +0500

    variables

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int8 = -1
+	var b uint8 = 2
+	var c byte = 3 // byte - синоним типа uint8
+	var d int16 = -4
+	var f uint16 = 5
+	var g int32 = -6
+	var h rune = -7 // rune - синоним типа int32
+	var j uint32 = 8
+	var k int64 = -9
+	var l uint64 = 10
+	var m int = 102
+	var n uint = 105
+
+	fmt.Println("a: ", a)
+	fmt.Println("b: ", b)
+	fmt.Println("c: ", c)
+	fmt.Println("d: ", d)
+	fmt.Println("f: ", f)
+	fmt.Println("g: ", g)
+	fmt.Println("h: ", h)
+	fmt.Println("j: ", j)
+	fmt.Println("k: ", k)
+	fmt.Println("l: ", l)
+	fmt.Println("m: ", m)
+	fmt.Println("n: ", n)
+}
